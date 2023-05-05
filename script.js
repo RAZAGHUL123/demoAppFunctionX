cfa6fb9906376f8af8d99e7fb0e65a3d63a3a39d
Rafael Moreno <92047284+RAZAGHUL123@users.noreply.github.com>
2023-05-04 19:22:05 -0700

    a change

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
+// This function takes an array of numbers and returns an array of the even numbers.
 function findEvenNumbers(numbers) {
 	const evenNumbers = [];
   
+	// Loop through the array of numbers.
 	for (let i = 0; i < numbers.length; i++) {
 	  const num = parseFloat(numbers[i]);
   
+	  // Check if the current value is not a number and skip it.
 	  if (isNaN(num)) {
 		console.log(`Invalid input: ${numbers[i]}. Skipping...`);
 		continue;
 	  }
   
+	  // Check if the current number is even and add it to the evenNumbers array.
 	  if (num % 2 === 0) {
 		evenNumbers.push(num);
 	  }
 	}
   
+	// If no even numbers were found, log a message and return null.
 	if (evenNumbers.length === 0) {
 	  console.log("No even numbers found.");
 	  return null;
 	} else {
+	  // Otherwise, return the array of even numbers.
 	  return evenNumbers;
-	}
-  }
-  
-  function findEven() {
-	const numbers = document.getElementById("numbers").value.split(",");
-	const evenNumbers = findEvenNumbers(numbers);
-  
-	if (evenNumbers !== null) {
-	  let outputHtml = "The even numbers are:<br>";
-	  for (let i = 0; i < evenNumbers.length; i++) {
-		outputHtml += `${evenNumbers[i]}<br>`;
-	  }
-	  document.getElementById("output").innerHTML = outputHtml;
 	}
   }
   
